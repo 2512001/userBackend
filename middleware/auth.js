8b16012a81a3db825b2d3bb86bb4f9baa976efdd
Deepak goswami <136834311+2512001@users.noreply.github.com>
2024-12-28 19:57:47 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,15 +2,5 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 exports.protect = async (req, res, next) => {
-  let  { token } = req.cookies;
-  console.log(token);
-  if (!token) return res.status(401).json({ message: 'No token, authorization denied' });
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findById(decoded.id).select('-password');
-    next();
-  } catch (error) {
-    res.status(401).json({ message: 'Token is not valid', error });
-  }
+ next();
 };
